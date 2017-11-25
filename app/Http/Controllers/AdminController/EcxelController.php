75701f6f1194a290cd06b7b276f36f7d4321dd89
tranluong96 <luongtvit96@gmail.com>
2017-11-25 12:24:40 +0700

    commit excel

---
+++ app/Http/Controllers/AdminController/EcxelController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\AdminController;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Excel;
+
+class EcxelController extends Controller
+{
+    public function ExportAddOrder()
+    {
+    	
+    }
+}
